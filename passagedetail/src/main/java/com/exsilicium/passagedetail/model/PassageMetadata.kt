5c045104df498ed98b5d47d2d939683ccb8bb449
Benjamin Wicks <benjaminlwicks@gmail.com>
2018-04-23 06:10:08 -0500

    WIP mock endpoint

--- passagedetail/src/main/java/com/exsilicium/passagedetail/model/PassageMetadata.kt
+++ passagedetail/src/main/java/com/exsilicium/passagedetail/model/PassageMetadata.kt
@@ -4,10 +4,10 @@ import com.google.gson.annotations.SerializedName
 
 internal data class PassageMetadata(
         val canonical: String,
-        @SerializedName("chapter_start") val chapterStart: List<Int>,
-        @SerializedName("chapter_end") val chapterEnd: List<Int>,
+        val chapterStart: List<Int>,
+        val chapterEnd: List<Int>,
         @SerializedName("prev_verse") val previousVerse: Int,
-        @SerializedName("next_verse") val nextVerse: Int,
+        val nextVerse: Int,
         @SerializedName("prev_chapter") val previousChapter: List<Int>,
-        @SerializedName("next_chapter") val nextChapter: List<Int>
+        val nextChapter: List<Int>
 )
