21c4d53e5a491c437be089c87e4b7a0ebc981bd5
Ben Wicks <bwicks@clientresourcesinc.com>
2018-04-27 10:46:00 -0500

    Fix merge accident

--- app/src/main/java/com/exsilicium/scripturememory/home/model/MemoryPassage.kt
+++
@@ -1,9 +0,0 @@
-package com.exsilicium.scripturememory.home.model
-
-import com.exsilicium.scripture.shared.model.ScriptureReference
-
-data class MemoryPassage(
-        val id: Int,
-        val scriptureReference: ScriptureReference,
-        val text: String
-)
