b10d9a6909b433e163f6caf1c1348be6b5a27873
Ben Wicks <bwicks@clientresourcesinc.com>
2018-07-20 16:45:45 -0500

    WIP begin widget
    https://developer.android.com/guide/topics/appwidgets/

---
+++ passageswidget/src/main/java/com/exsilicium/passageswidget/PassagesWidgetProvider.kt
@@ -0,0 +1,12 @@
+package com.exsilicium.passageswidget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+class PassagesWidgetProvider : AppWidgetProvider() {
+    override fun onUpdate(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetIds: IntArray?) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        // todo keep learning
+    }
+}
